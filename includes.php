2c57f581548658fa650271c83d7d4f8adf021e82
RickDias <rick_pereira_dias@hotmail.com>
2019-05-17 23:11:23 -0300

    vendor

---
+++ includes.php
@@ -0,0 +1,6 @@
+<?php
+require 'controller/core/ContentController.php';
+require 'controller/core/FooterController.php';
+require 'controller/core/HeaderController.php';
+require 'controller/AuthController.php';
+require 'controller/IndexController.php';
--- index.php
+++ index.php
@@ -3,7 +3,8 @@ echo "gerson";
 session_start();
 global $smarty, $context;
 // autoload do Composer
-// require 'vendor/autoload.php';
-$configuration = new Configuration(__DIR__);
+require 'vendor/autoload.php';
+// require 'includes.php';
+// $configuration = new Configuration(__DIR__);
 $index = new IndexController();
 ?>
