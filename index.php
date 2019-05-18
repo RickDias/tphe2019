4cbd44a5418e56d2815b63d5a1fec7f92ad4a051
RickDias <rick_pereira_dias@hotmail.com>
2019-05-17 22:07:32 -0300

    smarty add

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-//teste tphe 2019 
+<?php
+// session_start();
+// global $smarty, $context;
+// autoload do Composer
+// require 'vendor/autoload.php';
+// $configuration = new Configuration(__DIR__);
+// $index = new IndexController();
+?>
