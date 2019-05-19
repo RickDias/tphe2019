9696044d3e51fd5b0096d342c350492346e41bc1
RickDias <rick_pereira_dias@hotmail.com>
2019-05-19 20:06:07 -0300

    reinstall composer

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,10 +23,6 @@ class ComposerAutoloaderInit424f0f9fe40432e9f01ca219e6bc9682
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit424f0f9fe40432e9f01ca219e6bc9682', 'loadClassLoader'));
 
-        $includePaths = require __DIR__ . '/include_paths.php';
-        $includePaths[] = get_include_path();
-        set_include_path(implode(PATH_SEPARATOR, $includePaths));
-
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
--- vendor/dlundgren/css-splitter/tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * PHP CSS Splitter PhpUnit bootstrap
- *
- * @author    David Lundgren
- * @copyright 2014, David Lundgren. All Rights Reserved.
- * @license   MIT
- */
-require_once realpath(__DIR__ . '/../src/Splitter.php');
